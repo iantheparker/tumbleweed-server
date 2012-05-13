27d1d5d8321911d0fa502056a76be0031b692c90
Your Name <david.cascino@gmail.com>
2012-05-13 09:13:00 -0400

    added foursquare controller and some admin scaffolding

---
+++ app/controllers/foursquare_controller.rb
@@ -0,0 +1,33 @@
+require 'ruby-bitly'
+
+class FoursquareController < ApplicationController
+
+    def push
+      checkin = JSON.parse(params['checkin'])
+      logger.info(checkin)
+      venue = checkin["venue"]
+      venue_id = venue["id"]
+      venue_name = venue["name"]
+      contact = venue["contact"]
+
+      foursquare_user= JSON.parse(params['user'])
+      foursquare_user_id = foursquare_user["id"]
+
+      v = Venue.find_by_foursquare_id(venue_id)
+      if v.nil?
+        if contact.nil?
+          v = Venue.create(:foursquare_id => venue_id, :name => venue_name)
+        else
+          phone = contact["phone"]
+          twitter = contact["twitter"]
+          v = Venue.create(:foursquare_id => venue_id, :name => venue_name, :admin_phone => phone, :admin_twitter => twitter)
+        end
+      end
+
+      # TODO match up foursquare id to user's mobile device id (UDID?)
+      #checkin = Checkin.create(:venue_id => v.id, :user_id => user.id)
+
+      render :text => "got push"
+    end
+
+end
---
+++ db/migrate/20120513125951_create_checkins.rb
@@ -0,0 +1,10 @@
+class CreateCheckins < ActiveRecord::Migration
+  def change
+    create_table :checkins do |t|
+      t.integer :user_id
+      t.integer :venue_id
+
+      t.timestamps
+    end
+  end
+end
