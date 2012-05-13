d707d0946a5e327a34236e14a05366f74e9aad93
Your Name <david.cascino@gmail.com>
2012-05-13 08:08:55 -0400

    tumbleweed skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TumbleweedServer::Application.config.secret_token = 'a2f94b34e6b01d402be5e28d49bbac1f94101bbdc11bd310d60b9fe1f84c4130f93681ed72297b03203e92ee9adef78677ee7410d2a6339f7cbe94097ab6ecfa'
