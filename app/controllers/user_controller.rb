964721f20569cd71f5871d45658a2248cb11ec25
ian parker <ianrparker@gmail.com>
2012-07-10 22:52:03 -0400

    added user_controller and route

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,11 @@
+class UserController < ApplicationController
+
+    def register
+   render :text => "i'm in register" 
+   end
+
+
+    def status
+	render :text => "i'm in register"
+    end
+end
