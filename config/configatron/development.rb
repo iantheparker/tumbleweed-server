8fa1e991bae0945c538cec27ff1869ffb08121d6
Your Name <david.cascino@gmail.com>
2012-07-17 21:16:33 -0400

    test code and config changes

--- config/configatron/development.rb
+++ config/configatron/development.rb
@@ -2,7 +2,7 @@
 # 
 # Example:
 #   configatron.file.storage = :local
-configatron.apn.passphrase=''
-configatron.apn.feedback.passphrase=''
+configatron.apn.passphrase='tweed'
+configatron.apn.feedback.passphrase='tweed'
 configatron.apn.cert=File.join(Rails.root.to_s,"config","ck.pem")
 configatron.apn.feedback.cert=File.join(Rails.root.to_s,"config","ck.pem")
