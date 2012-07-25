dff8242550eb1e3304e6063d756cdde187ec2f3e
Your Name <david.cascino@gmail.com>
2012-07-24 21:41:38 -0400

    added user name logging

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,10 +3,18 @@ class UserController < ApplicationController
     def register
         @foursquare_id = params['foursquare_id']
         @device_id = params['device_token']
-        Rails.logger.info(@device_id)
-        Rails.logger.info(@foursquare_id)
-        @device=APN::Device.find_or_create_by_token(:token => @device_id)
-        Rails.logger.info(@device)
+        @user_name = params['user_name']
+        Rails.logger.info(@user_name)
+
+        @device=APN::Device.find_by_token(:token => @device_id)
+        if @device.nil?
+            @device = APN::Device.create(:token => @device_id)
+            # send our welcome push
+        else
+            # do other shit
+            Rails.logger.info("yo, device exists" + @device.inspect)
+        end
+
         @user = User.find_or_create_by_device_token_and_foursquare_id(@device.token, @foursquare_id)
         Rails.logger.info(@user)
         render :text => "i'm in register" 
