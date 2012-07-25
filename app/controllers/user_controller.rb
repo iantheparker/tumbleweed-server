34c66c7473e02c6b3211758143e8c27db5d49960
Your Name <david.cascino@gmail.com>
2012-07-24 21:09:15 -0400

    plumbing

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,11 +1,17 @@
 class UserController < ApplicationController
 
     def register
-   render :text => "i'm in register" 
-   end
+        @foursquare_id = params['foursquare_id']
+        @device_id = params['device_token']
+        Rails.logger.info(@device_id)
+        Rails.logger.info(@foursquare_id)
+        #@device=APN::Device.find_or_create_by_token(:token => @device_id)
+        #@user = User.find_by_token_and_foursquare_id(@device.token, @foursquare_id)
+        render :text => "i'm in register" 
+    end
 
 
     def status
-	render :text => "i'm in register"
+        render :text => "i'm in register"
     end
 end
