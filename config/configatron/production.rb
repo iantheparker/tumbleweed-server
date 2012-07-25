b516b5c7cadea32d312e576c91d176bbee9a762d
Your Name <david.cascino@gmail.com>
2012-07-24 22:30:39 -0400

    production config for push

--- config/configatron/production.rb
+++ config/configatron/production.rb
@@ -2,3 +2,7 @@
 # 
 # Example:
 #   configatron.file.storage = :s3
+configatron.apn.passphrase='tweed'
+configatron.apn.feedback.passphrase='tweed'
+configatron.apn.cert=File.join(Rails.root.to_s,"config","ck.pem")
+configatron.apn.feedback.cert=File.join(Rails.root.to_s,"config","ck.pem")
