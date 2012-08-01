d0b3861122162effd57fbef3e732b0530c764ad0
Your Name <david.cascino@gmail.com>
2012-07-31 20:11:07 -0400

    nil check

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,29 +5,34 @@ class UserController < ApplicationController
         @device_id = params['device_token']
         @first_name = params['first_name']
         @last_name = params['last_name']
+    
+        if @foursquare_id and @device_id and @first_name and @last_name
+            @device=APN::Device.find_by_token(@device_id)
+            if @device.nil?
+                @device = APN::Device.create(:token => @device_id)
+                # maybe send a notification about a new device
+            else
+                Rails.logger.info("yo, device exists" + @device.inspect)
+            end
 
-        @device=APN::Device.find_by_token(@device_id)
-        if @device.nil?
-            @device = APN::Device.create(:token => @device_id)
-            # maybe send a notification about a new device
+            @user = User.find_by_device_token_and_foursquare_id(@device.token, @foursquare_id)
+            if @user.nil?
+                @user = User.create(:device_token => @device.token, 
+                                    :foursquare_id => @foursquare_id,
+                                    :first_name => @first_name,
+                                    :last_name => @last_name
+                                   )
+                                   Rails.logger.info("oh snap, new user: send push")
+                                   send_push(@device, "Welcome to Tumbleweed " + @user.first_name)
+            else
+                Rails.logger.info("yo, user exists" + @user.inspect)
+            end
+
+            render :text => "i'm in register" 
         else
-            Rails.logger.info("yo, device exists" + @device.inspect)
+            render :text => "yo, pass in the variable foo" 
         end
 
-        @user = User.find_by_device_token_and_foursquare_id(@device.token, @foursquare_id)
-        if @user.nil?
-            @user = User.create(:device_token => @device.token, 
-                    :foursquare_id => @foursquare_id,
-                    :first_name => @first_name,
-                    :last_name => @last_name
-            )
-            Rails.logger.info("oh snap, new user: send push")
-            send_push(@device, "Welcome to Tumbleweed " + @user.first_name)
-        else
-            Rails.logger.info("yo, user exists" + @user.inspect)
-        end
-
-        render :text => "i'm in register" 
     end
 
 
