1a298669f82484d8108d15dd8e94b6caded659f5
Your Name <david.cascino@gmail.com>
2012-07-31 21:10:11 -0400

    added push to foursquare checkin

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -1,34 +1,57 @@
 class FoursquareController < ApplicationController
 
     def push
-      # first just log the raw checkin from foursquare
-      raw_checkin = RawCheckin.create(:payload => params['checkin'])
+        # first just log the raw checkin from foursquare
+        raw_checkin = RawCheckin.create(:payload => params['checkin'])
 
-      checkin = JSON.parse(params['checkin'])
-      logger.info(checkin)
-      venue = checkin["venue"]
-      venue_id = venue["id"]
-      venue_name = venue["name"]
-      contact = venue["contact"]
+        checkin = JSON.parse(params['checkin'])
 
-      foursquare_user= JSON.parse(params['user'])
-      foursquare_user_id = foursquare_user["id"]
+        logger.info(checkin)
 
-      v = Venue.find_by_foursquare_id(venue_id)
-      if v.nil?
-        if contact.nil?
-          v = Venue.create(:foursquare_id => venue_id, :name => venue_name)
-        else
-          phone = contact["phone"]
-          twitter = contact["twitter"]
-          v = Venue.create(:foursquare_id => venue_id, :name => venue_name, :admin_phone => phone, :admin_twitter => twitter)
+        venue = checkin["venue"]
+
+        venue_id = venue["id"]
+        venue_name = venue["name"]
+        contact = venue["contact"]
+
+        foursquare_user= JSON.parse(params['user'])
+        foursquare_user_id = foursquare_user["id"]
+
+        user = User.find_by_foursquare_id(foursquare_user_id)
+
+        if user
+            device = APN::Device.find_by_token(user.device_token)
+            message = "You checked in on foursquare " + venue_name
+            logger.info(message)
+            send_push(device, message)
         end
-      end
 
-      # TODO match up foursquare id to user's mobile device id (UDID?)
-      #checkin = Checkin.create(:venue_id => v.id, :user_id => user.id)
 
-      render :text => "got push"
+            v = Venue.find_by_foursquare_id(venue_id)
+            if v.nil?
+                if contact.nil?
+                    v = Venue.create(:foursquare_id => venue_id, :name => venue_name)
+                else
+                    phone = contact["phone"]
+                    twitter = contact["twitter"]
+                    v = Venue.create(:foursquare_id => venue_id, :name => venue_name, :admin_phone => phone, :admin_twitter => twitter)
+                end
+            end
+
+            # TODO match up foursquare id to user's mobile device id (UDID?)
+            #checkin = Checkin.create(:venue_id => v.id, :user_id => user.id)
+
+            render :text => "got push"
+        end
+
+        protected
+        def send_push(device, message)
+            notification=APN::Notification.new
+            notification.device=device
+            notification.badge=1
+            notification.sound=true
+            notification.alert=message
+            notification.save
+        end
+
     end
-
-end
