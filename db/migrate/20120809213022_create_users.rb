fdc1b83fa4e287bd4b0ac0e3cf37efad621ff20d
iantheparker <ianrparker@gmail.com>
2012-08-09 14:35:49 -0700

    added scaffolding for Users

--- db/migrate/20120809213022_create_users.rb
+++ db/migrate/20120809213022_create_users.rb
@@ -1,8 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.integer :id
       t.string :device_token
       t.string :foursquare_id
+      t.datetime :created_at
+      t.datetime :updated_at
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps
     end
