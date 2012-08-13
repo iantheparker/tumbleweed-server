11eb4e35e830040bb874fa54941c40cf1c638bb2
iantheparker <ianrparker@gmail.com>
2012-08-13 15:46:49 -0700

    p4

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -12,11 +12,11 @@ class FoursquareController < ApplicationController
         logger.info(checkin)
         logger.info(checkin_id)
         
-        devauth = "UT0L5SRHLHNCXFUNO3X4NKMIAFANLZBIWG13PA5F4N2L2F2M"
-        url = "https://api.foursquare.com/v2/checkins/"+checkin_id.to_s+"/reply"
-        uri = URI(url)
-		res = Net::HTTP.post_form(uri, 'oauth_token' => devauth, 'text' => 'Keep using Tumbleweed!')
-		puts res.body
+        #devauth = "UT0L5SRHLHNCXFUNO3X4NKMIAFANLZBIWG13PA5F4N2L2F2M"
+        #url = "https://api.foursquare.com/v2/checkins/"+checkin_id.to_s+"/reply"
+        #uri = URI(url)
+		#res = Net::HTTP.post_form(uri, 'oauth_token' => devauth, 'text' => 'Keep using Tumbleweed!')
+		#puts res.body
 
         #source = checkin["source"]
         #source_url = source["url"]
