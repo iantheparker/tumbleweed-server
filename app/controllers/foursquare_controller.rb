a18b1f9293d3566a0351ec03dcc8cc03dc713624
iantheparker <ianrparker@gmail.com>
2012-08-14 18:16:21 -0700

    p2

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -7,7 +7,7 @@ class FoursquareController < ApplicationController
         checkin = JSON.parse(params['checkin'])
         checkin_id = checkin["id"]
 
-        logger.info(checkin)
+        logger.info(raw_checkin)
 
         venue = checkin["venue"]
         venue_id = venue["id"]
@@ -27,7 +27,8 @@ class FoursquareController < ApplicationController
         	if source_url =~ /tumbleweed/
         		# /tumbleweed/.match(source_url)
         		puts "totally from tumbleweed"
-        		updateLevel(user.id)
+        		#update Level
+        		user.update_attributes(:level => (@user.level +=1))
         	else
         		#gamestate - does this checkin unlock the next level?
         		#checkin_reply(checkin_id, user.oauth_token) #with success message
@@ -56,13 +57,7 @@ class FoursquareController < ApplicationController
         
        	#render :json => @user.level 
     end
-    
-    update_level(id)
-    	@id = id
-    	@user = User.find_by_id(@id)
-       	@user.update_attributes(:level => (@user.level +=1))
-    end
-    
+ 
     def game_state
     	#if this game state && if checkin happened off of ios app && if checkin is under this parent category
     	#yes - update level, connected app message 'success', send push notification
@@ -71,50 +66,50 @@ class FoursquareController < ApplicationController
     end
     
     def checkin_source (checkin_id, params={}, oauth_token)
-      @checkin_id = checkin_id
-      params = {}.merge!(params)
+    	@checkin_id = checkin_id
+      	params = {}.merge!(params)
 
-      response = perform_graph_request("checkins/#{@checkin_id}", params, "get", oauth_token)
-      response = response["response"]
-      checkin = response["checkin"]
-      source = checkin["source"]
-      source_url = source["url"]
-      #render :json => source_url
-      logger.info(source_url)
-      return source_url
+      	response = perform_graph_request("checkins/#{@checkin_id}", params, "get", oauth_token)
+      	response = response["response"]
+      	checkin = response["checkin"]
+      	source = checkin["source"]
+      	source_url = source["url"]
+      	#render :json => source_url
+      	logger.info(source_url)
+      	return source_url
     end
     
     def checkin_reply(checkin_id, params={}, oauth_token)
-      @checkin_id = checkin_id
-      params = {:text => "Tumbleweed rules!",
-                :url => "tumbleweed://",
-                :v => "20120813"}.merge!(params)
+      	@checkin_id = checkin_id
+      	params = {:text => "Tumbleweed rules!",
+      		:url => "tumbleweed://",
+            :v => "20120813"}.merge!(params)
 
-      render :json => perform_graph_request("checkins/#{@checkin_id}/reply", params, "post", oauth_token)
+      	render :json => perform_graph_request("checkins/#{@checkin_id}/reply", params, "post", oauth_token)
     end
     
     def perform_graph_request(endpoint, params={}, method="get", oauth_token)
-      require 'net/http'
-      @access_token = oauth_token
-      @base_url = "https://api.foursquare.com:443/v2/"
+      	require 'net/http'
+      	@access_token = oauth_token
+      	@base_url = "https://api.foursquare.com:443/v2/"
 
-      @query_string = "?"
-      @query_string += "oauth_token=#{CGI.escape(@access_token)}" unless @access_token.empty?
+      	@query_string = "?"
+      	@query_string += "oauth_token=#{CGI.escape(@access_token)}" unless @access_token.empty?
 
-      if method=="get"
-        params.each{|key, val| @query_string += "&#{key}=#{val}"}
-        url = URI.parse("#{@base_url}#{endpoint}#{@query_string}")
-        request = Net::HTTP::Get.new("#{url.path}?#{url.query}",{"Content-Type"=>"text/json"})
-      else
-        url = URI.parse("#{@base_url}#{endpoint}#{@query_string}")
-        request = Net::HTTP::Post.new("#{url.path}?#{url.query}",{"Content-Type"=>"text/json"})
-        request.set_form_data(params)
-      end
+      	if method=="get"
+        	params.each{|key, val| @query_string += "&#{key}=#{val}"}
+        	url = URI.parse("#{@base_url}#{endpoint}#{@query_string}")
+        	request = Net::HTTP::Get.new("#{url.path}?#{url.query}",{"Content-Type"=>"text/json"})
+      	else
+        	url = URI.parse("#{@base_url}#{endpoint}#{@query_string}")
+        	request = Net::HTTP::Post.new("#{url.path}?#{url.query}",{"Content-Type"=>"text/json"})
+        	request.set_form_data(params)
+      	end
 
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
-      response = JSON.parse(http.start {|http| http.request(request)}.body)
-      return response
+      	http = Net::HTTP.new(url.host, url.port)
+      	http.use_ssl = true
+      	response = JSON.parse(http.start {|http| http.request(request)}.body)
+      	return response
     end
 
     protected     
