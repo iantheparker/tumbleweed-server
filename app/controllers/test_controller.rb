49f4ba2584019ed53598e36c8de3f5701412528a
iantheparker <ianrparker@gmail.com>
2012-09-11 22:11:09 -0400

    new prod cert

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,8 +1,8 @@
 class TestController < ApplicationController
   def createapn
-  	user = User.last
+  	user = User.first
   	message = "hello world"
-  	device = user.device_token
+  	device = APN::Device.find_by_token(user.device_token)
   	user.send_push(device, message)
   	render :text => "ok"
   end
